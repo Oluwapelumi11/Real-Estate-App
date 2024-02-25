c2976f1b377f16fac22f019d5f77094906f71c86
oluwapelumi11 <oluwapelumiezeliel11@gmail.com>
2024-02-25 16:54:16 +0100

    updating icos

--- src/app/smallComponents/member/member.component.ts
+++ src/app/smallComponents/member/member.component.ts
@@ -10,7 +10,14 @@ import { Agent } from '../../agent';
       <img [src]="agent.image" class="w-full h-[90%]" alt="">
       <div class="text-center py-3">
         <p class="uppercase font-semibold">{{agent.name}}</p>
-        <p class="text-gray-600 text-sm  py-1">{{agent.email}}</p>
+        <div class="flex gap-5  ">
+          <a [href]="agent.facebook">
+            
+          </a>
+          <a [href]="agent.whatsapp">
+            
+            </a>
+        </div>
       </div>
     </div>
   `,
