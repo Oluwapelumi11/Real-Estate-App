9a4c5c0ed000f374cc4e545ef34575935bbc5c2b
oluwapelumi11 <oluwapelumiezeliel11@gmail.com>
2024-03-03 11:01:51 +0100

    added fixe

--- src/app/smallComponents/buttoned-carousel/buttoned-carousel.component.ts
+++ src/app/smallComponents/buttoned-carousel/buttoned-carousel.component.ts
@@ -11,11 +11,11 @@ import { animate, style, transition, trigger } from '@angular/animations';
   ],
   template: `
   <div [@slideAnimation]="currentIndex" class="w-full h-full  relative carousel"  [ngStyle]="{'background-image':getCurrentSlideUrl()}">
-    <div class=" absolute flex justify-between sm:px-8 px-3 text-4xl w-full z-10 items-center h-full" >
-    <button class="text-white font-bold "  (click)="goToPrev()"><svg xmlns="http://www.w3.org/2000/svg" width="80" height="110" fill="currentColor" class="bi bi-chevron-compact-left w-24 h-36" viewBox="0 0 16 16">
+    <div class=" absolute sm:flex hidden justify-between px-8 text-4xl w-full z-10 items-center h-full" >
+    <button class="text-white font-bold "  (click)="goToPrev()"><svg xmlns="http://www.w3.org/2000/svg" width="80" height="110" fill="currentColor" class="bi bi-chevron-compact-left " viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M9.224 1.553a.5.5 0 0 1 .223.67L6.56 8l2.888 5.776a.5.5 0 1 1-.894.448l-3-6a.5.5 0 0 1 0-.448l3-6a.5.5 0 0 1 .67-.223"/>
 </svg></button>
-    <button class="text-white font-bold " (click)="goToNext()"><svg xmlns="http://www.w3.org/2000/svg" width="80" height="110" fill="currentColor" class="bi bi-chevron-compact-right w-24 h-36" viewBox="0 0 16 16">
+    <button class="text-white font-bold " (click)="goToNext()"><svg xmlns="http://www.w3.org/2000/svg" width="80" height="110" fill="currentColor" class="bi bi-chevron-compact-right " viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M6.776 1.553a.5.5 0 0 1 .671.223l3 6a.5.5 0 0 1 0 .448l-3 6a.5.5 0 1 1-.894-.448L9.44 8 6.553 2.224a.5.5 0 0 1 .223-.671"/>
 </svg></button>
     </div>
